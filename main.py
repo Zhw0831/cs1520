12903e0752d0432caf0609baa9c7003668d93152
Zhw0831 <61070056+Zhw0831@users.noreply.github.com>
2021-09-27 21:27:18 -0400

    minor change to make it work

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def root():
-    return flask.redirect("/s/index.html", code=302)
+    return flask.redirect("/s/login.html", code=302)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
