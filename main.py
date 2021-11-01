6598029e2a68d13b4e2b789fc0d89cb1e0a6d337
wowotou666 <wew92@pitt.edu>
2021-10-31 20:58:37 -0400

    update quiz

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import flask
 
-
 app = flask.Flask(__name__)
+
 
 @app.route('/')
 def root():
     return flask.redirect("/s/login.html", code=302)
+
+
+@app.route('/questions/<id>')
+def questions(id):
+    # TODO fetch data from database, removing the following lanes later
+    choices_ans = {
+        "choice": ["A", "B", "C","D"]
+    }
+
+    return flask.render_template("quiz.html", choices=choices_ans)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
