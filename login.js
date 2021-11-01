4c16725e4cb8e21ff0db43c84685fcc3fdf732bc
Connor Clarke <cmc240@pitt.edu>
2021-10-31 19:03:32 -0700

    more firebase

--- login.js
+++ login.js
@@ -1,16 +1,29 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+var provider = new firebase.auth.GoogleAuthProvider();
 
-const signInWithGoogle = document.getElementById('login');
-const auth = firebase.auth();
-
-const signInWithGoogle = () => {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(googleProvider)
-    .then(() => {
-        window.location.assign('./login')
-    })
-    .catch(error => {
-        console.error(error);
-    })
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
 }
-signInWithGoogle.addEventListener('click', signInWithGoogle);
+
+function googleSignout() {
+   firebase.auth().signOut()
+	
+   .then(function() {
+      console.log('Signout Succesfull')
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
