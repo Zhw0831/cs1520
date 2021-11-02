187a057567b63d206da58b096ea5d55c97cc9c6e
Connor Clarke <cmc240@pitt.edu>
2021-11-02 10:16:11 -0400

    db now stores emails

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ from google.cloud import datastore
 
 app = flask.Flask(__name__)
 
+def get_client():
+    return datastore.Client()
+
+def create_thing():
+    client = get_client()
+    key = client.key('User_Emails:')
+    return datastore.Entity(key)
+
+def update_thing(thing):
+    client = get_client()
+    client.put(thing)
+
 @app.route('/')
 def root():
     return flask.redirect('/s/welcome.html', code=302)
 
-@app.route('/login.html')
+@app.route('/login.html', methods=['GET','POST'])
 def login():
     return flask.render_template('login.html')
 
@@ -26,6 +38,14 @@ def questions(id):
         )
 
     return flask.render_template("quiz.html", choices=choices_ans[int(id)],id = id)
+
+@app.route('/create-thing', methods=['GET', 'POST'])
+def store():
+    email = flask.request.values['email']
+    thing = create_thing()
+    thing['email'] = email
+    update_thing(thing)
+    return login()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
