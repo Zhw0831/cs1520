7584fb9d1858dc8810253125e7bfd07bc5cdea5f
Luke Repasky <lukerepasky@Lukes-MacBook-Pro.local>
2021-12-08 23:33:42 -0500

    reverted main to old

--- main.py
+++ main.py
@@ -1,67 +1,57 @@
 import flask
 import random
-import user
+from flask import request
 from google.cloud import datastore
-from flask import flash, redirect, render_template, request, url_for
-from user import get_client
-um = user.User_manager()
+
 
 app = flask.Flask(__name__)
-client = get_client()
 
+def get_client():
+    return datastore.Client()
+
+def create_thing():
+    client = get_client()
+    key = client.key('User')
+    return datastore.Entity(key)
+
+def update_thing(thing):
+    client = get_client()
+    client.put(thing)
 
 @app.route('/')
 def root():
-    return flask.redirect("/s/welcome.html", code=302)
+    return flask.redirect('/s/welcome.html', code=302)
 
-@app.route('/send', methods=['POST','GET'])
-def register_user():
-    email = flask.request.form['email']
-    password = flask.request.form['password']
-    age = flask.request.form['age']
-    gender = flask.request.form['gender']
-   
-
-    um.register(email, password, age, gender) 
-    print("You have successfully signed up with email", email)
-    return flask.render_template("user.html", email = email, age = age, gender = gender)
-
-# @app.route('/user.html', methods=['POST', 'GET'])
-# def personal():
-#     return flask.render_template('user.html')
-
-@app.route('/login.html', methods=['POST','GET'])
+@app.route('/login.html', methods=['GET','POST'])
 def login():
     return flask.render_template('login.html')
 
-@app.route('/login', methods=['POST','GET'])
-def store_login():
+@app.route('/signup.html', methods=['GET','POST'])
+def signup():
+    return flask.render_template('signup.html')
+
+@app.route('/questions/<id>')
+def questions(id):
+    # TODO fetch data from database, removing the following lanes later
+    choices_ans = []
+    for i in range(101):
+        choices_ans.append(
+            {
+                "choice": [random.random(), random.random(), random.random(), random.random()]
+            }
+        )
+
+    return flask.render_template("quiz.html", choices=choices_ans[int(id)],id = id)
+
+@app.route('/create-thing', methods=['GET', 'POST'])
+def store():
     email = flask.request.form['email']
     password = flask.request.form['password']
-    response = um.login(email, password) 
-    print(response)
-
-    age = response['age'] 
-    gender = response['gender'] 
-    return flask.render_template("user.html", email = email, age = age, gender = gender)
-
-@app.route('/signup.html', methods=['POST','GET'])
-def signup():
-    return render_template('signup.html')
-
-
-# @app.route('/questions/<id>')
-# def questions(id):
-#     # TODO fetch data from database, removing the following lanes later
-#     choices_ans = []
-#     for i in range(101):
-#         choices_ans.append(
-#             {
-#                 "choice": [random.random(), random.random(), random.random(), random.random()]
-#             }
-#         )
-
-#     return flask.render_template("quiz.html", choices=choices_ans[int(id)],id = id)
+    thing = create_thing()
+    thing['email'] = email
+    thing['password'] = password
+    update_thing(thing)
+    return login()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
